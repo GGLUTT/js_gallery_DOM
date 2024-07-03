f7f7101ce1ecf1dc2557263babe91e98dc4ce829
luter <newgglutt@gmail.com>
2024-07-03 16:08:28 +0300

    make build

---
+++ index.182ccc6e.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.getElementById("thumbs"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){e.preventDefault();var n=e.target;if("IMG"===n.tagName){var a=n.closest("a");a&&(t.src=a.href)}})});
+//# sourceMappingURL=index.182ccc6e.js.map
